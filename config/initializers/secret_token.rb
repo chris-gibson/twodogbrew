34ccbdc01cfb11e75c05674557431b3681295235
unknown <chris@hb03.(none)>
2012-05-23 22:45:36 -0500

    Adding initial files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twodogbrew::Application.config.secret_token = '7ad336a3fd4e8d4f38c99281a6efe4707f8979417e3b9f7e775bad2ffa1cbe291b99b5724a49641a3cab31722be2fb9394682f6a98b044f953443e7ad05de992'
