aa76164f75494d9b1d2e9d52c84558e3194b4e33
Chris Gibson <chris@twodogbrew.com>
2012-05-24 18:19:59 -0500

    Configuring some things

--- Gemfile
+++ Gemfile
@@ -1,6 +1,10 @@
+require "rubygems"
+require "mongo"
+
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.3'
+gem 'mongo_mapper'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
---
+++ config/initializers/mongo.rb
@@ -0,0 +1,10 @@
+class Mongo
+  MongoMapper.connection = Mongo::Connection.new('tester',48001)
+  MongoMapper.database = "#myapp-#{Rails.env}"
+
+  if defined?(PhusionPassenger)
+    PhusionPassenger.on_event(:starting_worker_process)     do |forked|
+      MongoMapper.connection.connect if forked
+    end
+  end
+end
