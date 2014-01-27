507d3dea440475ed7dfc71bd65a5a2a3bdaa0ae0
Chris Gibson <chris.david.gibson@gmail.com>
2014-01-26 19:15:17 -0600

    Adding static folder

--- src/server/app.js
+++ src/server/app.js
@@ -2,9 +2,13 @@ var express = require("express");
 var app = express();
 app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Testing');
-});
+var publicDir = path.join(path.dirname(__dirname), '/public/');
+
+app.use(express.static(publicDir));
+
+//app.get('/', function(request, response) {
+//  response.send('Testing');
+//});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
